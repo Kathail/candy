30da4fa92c9cc7bece874604683d3ef6f241beec
Kathail <209044134+Kathail@users.noreply.github.com>
2026-01-29 02:30:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    "DATABASE_URL", "sqlite:///candy_route.db"
+    "DATABASE_URL", "sqlite:///instance/candy_route.db"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
